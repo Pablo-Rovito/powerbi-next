b7b731d19515535f56ad5ec53002af87e4f53036
Pablo Rovito <pablo.rovito@outlook.com>
2022-05-10 10:22:27 -0300

    attempted fix to deploy 3
    Error: Module not found: Can't resolve 'next/Image' in '/vercel/path0/pages'
    Solution: checked case use in next/image module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
-import Image from 'next/Image';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
